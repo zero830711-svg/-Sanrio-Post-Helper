4ae118ea6d343018b24f5c04312d1242e61c8b01
zero830711-svg <zero830711@gmail.com>
2026-09-26 19:05:43 +0900

    アプリ内部のバージョン番号を同期

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function characterDefForQuery(query){
 }
 
 let trendRangeHours=24;
-const APP_VERSION="2026.09.26-3346";
+const APP_VERSION="2026.09.26-3347";
 let archiveFilter="all";
 let archiveView="posts";
 let separatedProductIds=new Set();
